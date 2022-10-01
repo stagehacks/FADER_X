ddd177019bdfc42d82fafdfcbb0f373d5ff130a9
sparks-alec <sparks.alec@gmail.com>
2022-09-30 23:55:17 -0400

    prettier dancing

--- FADER_X/Dance.ino
+++ FADER_X/Dance.ino
@@ -1,23 +1,32 @@
+boolean zeroFlag = false;
 void danceLoop(){
-  if(globalClock<2000){
-    setFaderTarget(fader1.getChannel(), (sin(globalClock/256.0)+1)*512);
-    setFaderTarget(fader2.getChannel(), (sin((globalClock-200)/256.0)+1)*512);
-    setFaderTarget(fader3.getChannel(), (sin((globalClock-400)/256.0)+1)*512);
-    setFaderTarget(fader4.getChannel(), (sin((globalClock-600)/256.0)+1)*512);
-    setFaderTarget(fader5.getChannel(), (sin((globalClock-800)/256.0)+1)*512);
-    setFaderTarget(fader6.getChannel(), (sin((globalClock-1000)/256.0)+1)*512);
-    setFaderTarget(fader7.getChannel(), (sin((globalClock-1200)/256.0)+1)*512);
-    setFaderTarget(fader8.getChannel(), (sin((globalClock-1400)/256.0)+1)*512);
+  if(globalClock<2500){
+    setFaderTarget(fader1.getChannel(), dancePos(0));
+    setFaderTarget(fader2.getChannel(), dancePos(200));
+    setFaderTarget(fader3.getChannel(), dancePos(400));
+    setFaderTarget(fader4.getChannel(), dancePos(600));
+    setFaderTarget(fader5.getChannel(), dancePos(800));
+    setFaderTarget(fader6.getChannel(), dancePos(1000));
+    setFaderTarget(fader7.getChannel(), dancePos(1200));
+    setFaderTarget(fader8.getChannel(), dancePos(1400));
     
-  }else if(globalClock>2000 && globalClock<2100){
+  }else if(globalClock>2500 && !zeroFlag){
+    zeroFlag = true;
+    Serial.println("zero");
     for(byte i=0; i<255; i++){
       globalFaderTargets[i] = 0;
     }
     
-  }else if(globalClock>2100){
+  }else if(globalClock>2500){
     pauseAllFaders();
     globalMode = EEPROM.read(24);
-    globalClock = -5000;
+    globalClock = 0;
     unpauseAllFaders();
   }
+}
+
+
+int dancePos(int mod){
+  int c = min(max((int)globalClock-mod, 0), 1000)-400;
+  return (sin(c/128.0)+1)*512;
 }
