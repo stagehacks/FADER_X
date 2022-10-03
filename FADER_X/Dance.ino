0cc2d2acbd5ebb7d9ef30628483c2cee62059de5
sparks-alec <sparks.alec@gmail.com>
2022-10-02 20:36:11 -0400

    totally new dance moves

--- FADER_X/Dance.ino
+++ FADER_X/Dance.ino
@@ -2,23 +2,24 @@ boolean zeroFlag = false;
 void danceLoop(){
   long mils = millis()-globalLastBoot;
   
-  if(mils<2500){
-    setFaderTarget(fader1.getChannel(), dancePos(mils-0));
-    setFaderTarget(fader2.getChannel(), dancePos(mils-200));
-    setFaderTarget(fader3.getChannel(), dancePos(mils-400));
-    setFaderTarget(fader4.getChannel(), dancePos(mils-600));
-    setFaderTarget(fader5.getChannel(), dancePos(mils-800));
-    setFaderTarget(fader6.getChannel(), dancePos(mils-1000));
-    setFaderTarget(fader7.getChannel(), dancePos(mils-1200));
-    setFaderTarget(fader8.getChannel(), dancePos(mils-1400));
+  if(mils<600 && mils%5==0){
+    zeroFlag = false;
+    setFaderTarget(fader1.getChannel(), mils*3);
+    setFaderTarget(fader2.getChannel(), mils*3-100);
+    setFaderTarget(fader3.getChannel(), mils*3-200);
+    setFaderTarget(fader4.getChannel(), mils*3-300);
+    setFaderTarget(fader5.getChannel(), mils*3-400);
+    setFaderTarget(fader6.getChannel(), mils*3-500);
+    setFaderTarget(fader7.getChannel(), mils*3-600);
+    setFaderTarget(fader8.getChannel(), mils*3-700);
     
-  }else if(mils>2500 && !zeroFlag){
+  }else if(mils>600 && !zeroFlag){
     zeroFlag = true;
     for(byte i=0; i<255; i++){
-      globalFaderTargets[i] = 0;
+      setFaderTarget(i, 0);
     }
     
-  }else if(mils>2500){
+  }else if(mils>600){
     pauseAllFaders();
     globalMode = EEPROM.read(24);
     globalLastBoot = 0;
@@ -28,6 +29,6 @@ void danceLoop(){
 
 
 int dancePos(int mod){
-  int c = min(max(mod, 0), 1000)-400;
-  return (sin(c/128.0)+1)*512;
+  int c = min(max(mod, 0), 1000);
+  return c;//(sin(c/128.0)+1)*512;
 }
