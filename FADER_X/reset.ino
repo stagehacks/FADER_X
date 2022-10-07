304f6b7105ec46d2177ec6b353828f8bad85f04a
sparks-alec <sparks.alec@gmail.com>
2022-10-06 22:51:39 -0400

    update version number

--- FADER_X/reset.ino
+++ FADER_X/reset.ino
@@ -147,7 +147,7 @@ void factoryReset(){
 
   EEPROM.write(8, 0);
   EEPROM.write(9, 8);
-  EEPROM.write(10, 0);
+  EEPROM.write(10, 1);
 
   EEPROM.put(14, defaultMessageWaitMillis); // Message wait (16 bit)
   EEPROM.write(16, defaultMotorMinSpeed); // Motor Min Speed
